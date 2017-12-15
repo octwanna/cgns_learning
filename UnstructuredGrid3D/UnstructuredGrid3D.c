647d2b91a258115e1ee637d42577b3c90e1f50a7
Lucas Guesser Targino da Silva <guesser@sinmec.ufsc.br>
2017-12-15 17:45:05 -0200

    Unstructured grid generator code refactored.
    'UnstructuredGrid3D.c' changed to call functions.

--- UnstructuredGrid3D/UnstructuredGrid3D.c
+++ UnstructuredGrid3D/UnstructuredGrid3D.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <cgnslib.h>
 
 #define CHKERRQ(err) if((err)) cg_error_exit()
@@ -7,47 +8,64 @@
 #define PHYSICAL_DIMENSION 3
 #define CELL_DIMENSION 3
 
-#define NX 21
-#define NY 17
-#define NZ 9
+#define MAX_STRING_LEN 200
+
+typedef struct cgns_unstructured_file
+{
+	char fileName[MAX_STRING_LEN];
+	char baseName[MAX_STRING_LEN];
+	char zoneName[MAX_STRING_LEN];
+	char sectionName[MAX_STRING_LEN];
+
+	int nx, ny, nz;
+
+	int file, base, zone, section;
+	int cellDimension, physicalDimension;
+
+	int coorX, coorY, coorZ;
+} cgns_unstructured_file;
+
+void generate_zone(cgns_unstructured_file *data);
+void generateCoordinates(cgns_unstructured_file *data);
+void generateElementsConnectivity(cgns_unstructured_file *data);
 
 int main(int argc, char *argv[])
 {
 	int err;
-	int i, j, k, entry, elementNumber;
+	cgns_unstructured_file data;
 
-	int cellDimension = CELL_DIMENSION;
-	int physicalDimension = PHYSICAL_DIMENSION;
+	strcpy(data.fileName, "/home/guesser/cgns_examples/output/UnstructuredGrid3D.cgns");
+	strcpy(data.baseName, "Simple structured grid");
+	strcpy(data.zoneName, "Grid");
+	strcpy(data.sectionName, "Hexaedron conectivity");
 
-	int file;
-	char fileName[] = "/home/guesser/cgns_examples/output/UnstructuredGrid3D.cgns";
+	data.nx = 21;
+	data.ny = 17;
+	data.nz = 9;
 
-	int base;
-	char baseName[] = "Simple structured grid";
+	data.cellDimension = CELL_DIMENSION;
+	data.physicalDimension = PHYSICAL_DIMENSION;
 
-	int zone;
-	char zoneName[] = "Grid";
+	err = cg_open(data.fileName, CG_MODE_WRITE, &(data.file)); CHKERRQ(err);
+	err = cg_base_write(data.file, data.baseName, data.cellDimension, data.physicalDimension, &(data.base)); CHKERRQ(err);
+	generate_zone(&data);
+	generateCoordinates(&data);
+	generateElementsConnectivity(&data);
+
+	cg_close(data.file);
+
+	printf("\nSuccessfully wrote grid to file '%s'\n\n", data.fileName);
+
+	exit(EXIT_SUCCESS);
+}
+
+void generate_zone(cgns_unstructured_file *data)
+{
+	int err;
 	cgsize_t size[3];
-
-	double *x=NULL, *y=NULL, *z=NULL;
-	int coorX, coorY, coorZ;
-	char coorXName[] = "CoordinateX";
-	char coorYName[] = "CoordinateY";
-	char coorZName[] = "CoordinateZ";
-
-	int section;
-	char sectionName[] = "Hexaedron conectivity";
-
-	int numberOfElements;
-	cgsize_t *hexaedronConnectivity;
-	cgsize_t firstElementNumber = 0;
-	cgsize_t lastElementNumber;
-
-	/* Open file */
-	err = cg_open(fileName, CG_MODE_WRITE, &file); CHKERRQ(err);
-
-	/* Open base */
-	err = cg_base_write(file, baseName, cellDimension, physicalDimension, &base); CHKERRQ(err);
+	const int NX = data->nx;
+	const int NY = data->ny;
+	const int NZ = data->nz;
 
 	/* Define zone size */
 	size[0] = NX*NY*NZ;
@@ -55,12 +73,32 @@ int main(int argc, char *argv[])
 	size[2] = 0;
 
 	/* Open zone */
-	err = cg_zone_write(file, base, zoneName, size, Unstructured, &zone);
+	err = cg_zone_write(data->file, data->base, data->zoneName, size, Unstructured, &(data->zone)); CHKERRQ(err);
+
+	return;
+}
+
+void generateCoordinates(cgns_unstructured_file *data)
+{
+	int err;
+	int i, j, k, entry;
+	const int NX = data->nx;
+	const int NY = data->ny;
+	const int NZ = data->nz;
+
+	double * x;
+	double * y;
+	double * z;
+
+	char coorXName[] = "CoordinateX";
+	char coorYName[] = "CoordinateY";
+	char coorZName[] = "CoordinateZ";
 
 	/* Alloc coordinates */
 	x = (double *) malloc(NX*NY*NZ*sizeof(double**));
 	y = (double *) malloc(NX*NY*NZ*sizeof(double**));
 	z = (double *) malloc(NX*NY*NZ*sizeof(double**));
+
 	/* Generate coordinates values */
 	for(i=0 ; i<NX ; ++i)
 	{
@@ -76,9 +114,27 @@ int main(int argc, char *argv[])
 		}
 	}
 	/* Write coordinates */
-	err = cg_coord_write(file, base, zone, CGNS_ENUMV(RealDouble), coorXName, x, &coorX); CHKERRQ(err);
-	err = cg_coord_write(file, base, zone, CGNS_ENUMV(RealDouble), coorYName, y, &coorY); CHKERRQ(err);
-	err = cg_coord_write(file, base, zone, CGNS_ENUMV(RealDouble), coorZName, z, &coorZ); CHKERRQ(err);
+	err = cg_coord_write(data->file, data->base, data->zone, CGNS_ENUMV(RealDouble), coorXName, x, &(data->coorX)); CHKERRQ(err);
+	err = cg_coord_write(data->file, data->base, data->zone, CGNS_ENUMV(RealDouble), coorYName, y, &(data->coorY)); CHKERRQ(err);
+	err = cg_coord_write(data->file, data->base, data->zone, CGNS_ENUMV(RealDouble), coorZName, z, &(data->coorZ)); CHKERRQ(err);
+
+	free(x);
+	free(y);
+	free(z);
+}
+
+void generateElementsConnectivity(cgns_unstructured_file *data)
+{
+	int i, j, k;
+	int numberOfElements, elementNumber, firstVerticeIndex;
+	cgsize_t *hexaedronConnectivity;
+	cgsize_t firstElementNumber = 1;
+	cgsize_t lastElementNumber;
+
+	const int NX = data->nx;
+	const int NY = data->ny;
+	const int NZ = data->nz;
+
 
 	/* Elements conectivity */
 		/* 0 <= elementNumber <= (NX-1)*(NY-1)*(NZ-1) */
@@ -91,31 +147,20 @@ int main(int argc, char *argv[])
 			for(k=0 ; k<(NZ-1) ; ++k)
 			{
 				elementNumber = i + (NX-1)*j + (NX-1)*(NY-1)*k;
-				entry = i + j*NX + k*NX*NY;
-				printf("elementNumber = %d\n", elementNumber);
-				hexaedronConnectivity[8*elementNumber+0] = 1 + entry;
-				hexaedronConnectivity[8*elementNumber+1] = 1 + entry+1;
-				hexaedronConnectivity[8*elementNumber+2] = 1 + entry+NX+1;
-				hexaedronConnectivity[8*elementNumber+3] = 1 + entry+NX;
-				hexaedronConnectivity[8*elementNumber+4] = 1 + entry+NX*NY;
-				hexaedronConnectivity[8*elementNumber+5] = 1 + entry+NX*NY+1;
-				hexaedronConnectivity[8*elementNumber+6] = 1 + entry+NX*(NY+1)+1;
-				hexaedronConnectivity[8*elementNumber+7] = 1 + entry+NX*(NY+1);
+				firstVerticeIndex = i + j*NX + k*NX*NY;
+				hexaedronConnectivity[8*elementNumber+0] = 1 + firstVerticeIndex;
+				hexaedronConnectivity[8*elementNumber+1] = 1 + firstVerticeIndex + 1;
+				hexaedronConnectivity[8*elementNumber+2] = 1 + firstVerticeIndex + NX + 1;
+				hexaedronConnectivity[8*elementNumber+3] = 1 + firstVerticeIndex + NX;
+				hexaedronConnectivity[8*elementNumber+4] = 1 + firstVerticeIndex + NX*NY;
+				hexaedronConnectivity[8*elementNumber+5] = 1 + firstVerticeIndex + NX*NY + 1;
+				hexaedronConnectivity[8*elementNumber+6] = 1 + firstVerticeIndex + NX*(NY+1) + 1;
+				hexaedronConnectivity[8*elementNumber+7] = 1 + firstVerticeIndex + NX*(NY+1);
 			}
 		}
 	}
 	lastElementNumber = numberOfElements;
-	cg_section_write(file, base, zone, sectionName, CGNS_ENUMV(HEXA_8), firstElementNumber+1, lastElementNumber, 0, hexaedronConnectivity, &section);
-
-
-
-	cg_close(file);
+	cg_section_write(data->file, data->base, data->zone, data->sectionName, CGNS_ENUMV(HEXA_8), firstElementNumber, lastElementNumber, 0, hexaedronConnectivity, &(data->section));
 
 	free(hexaedronConnectivity);
-	free(x);
-	free(y);
-	free(z);
-	printf("\nSuccessfully wrote grid to file '%s'\n\n", fileName);
-
-	exit(EXIT_SUCCESS);
 }
