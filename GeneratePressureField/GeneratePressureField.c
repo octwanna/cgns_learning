fedae9fce72ed492f49384ac77aaf2876b03c2c1
Lucas Guesser Targino da Silva <guesser@sinmec.ufsc.br>
2018-01-16 17:41:15 -0200

    GeneratePressureField improved to accept grids with any size.

--- GeneratePressureField/GeneratePressureField.c
+++ GeneratePressureField/GeneratePressureField.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
 
 	cgsize_t range_min, range_max;
 
-	int entry, i, j, k, NX=10, NY=15, NZ=20;
+	int entry, count;
 	double *pressure;
 
 	int field;
@@ -30,20 +30,6 @@ int main(int argc, char *argv[])
 	gridNumber = 1;
 	err = cg_grid_read(data.file, data.base, data.zone, gridNumber, gridName); CHKERRQ(err);
 
-	/* Create pressure field */
-	pressure = (double *) malloc(data.size[0]*sizeof(double));
-	for(i=0 ; i<NX ; ++i)
-	{
-		for(j=0 ; j<NY ; ++j)
-		{
-			for(k=0 ; k<NZ ; ++k)
-			{
-				entry = i + j*NX + k*NX*NY;
-				pressure[entry] = (double) entry;
-			}
-		}
-	}
-
 	/* Read coordinates */
 	err = cg_ncoords(data.file, data.base, data.zone, &numberOfCoordinates); CHKERRQ(err);
 	x = (double *) malloc(data.size[0]*sizeof(double));
@@ -51,16 +37,23 @@ int main(int argc, char *argv[])
 	z = (double *) malloc(data.size[0]*sizeof(double));
 	range_min = 1;
 	range_max = data.size[0];
-	err = cg_coord_read(data.file, data.base, data.zone, "CoordinateX",CGNS_ENUMV(RealDouble), &range_min, &range_max, x); CHKERRQ(err);
-	err = cg_coord_read(data.file, data.base, data.zone, "CoordinateY",CGNS_ENUMV(RealDouble), &range_min, &range_max, y); CHKERRQ(err);
-	err = cg_coord_read(data.file, data.base, data.zone, "CoordinateZ",CGNS_ENUMV(RealDouble), &range_min, &range_max, z); CHKERRQ(err);
+	err = cg_coord_read(data.file, data.base, data.zone, "CoordinateX", CGNS_ENUMV(RealDouble), &range_min, &range_max, x); CHKERRQ(err);
+	err = cg_coord_read(data.file, data.base, data.zone, "CoordinateY", CGNS_ENUMV(RealDouble), &range_min, &range_max, y); CHKERRQ(err);
+	err = cg_coord_read(data.file, data.base, data.zone, "CoordinateZ", CGNS_ENUMV(RealDouble), &range_min, &range_max, z); CHKERRQ(err);
+
+	/* Create pressure field */
+	pressure = (double *) malloc(data.size[0]*sizeof(double));
+	for(count=0 ; count<data.size[0] ; ++count)
+	{
+		pressure[count] = (double) count;
+	}
 
 	/* Write field */
 	err = cg_sol_write(data.file, data.base, data.zone, data.solutionName, CGNS_ENUMV(Vertex), &(data.solution)); CHKERRQ(err);
 	err = cg_field_write(data.file, data.base, data.zone, data.solution, CGNS_ENUMV(RealDouble), "Pressure", pressure, &field); CHKERRQ(err);
 
-
 	/* Close file and program */
 	cg_close(data.file);
+	free(pressure);
 	exit(EXIT_SUCCESS);
 }
