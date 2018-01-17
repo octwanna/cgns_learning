128267a386370821c992e0a50c063dfc68d137a8
Lucas Guesser Targino da Silva <guesser@sinmec.ufsc.br>
2018-01-17 08:04:00 -0200

    GenerateTransientPressureField completed and working with paraview.
    The file needs to be refactored.

--- GenerateTransientPressureField/GenerateTransientPressureField.c
+++ GenerateTransientPressureField/GenerateTransientPressureField.c
@@ -14,10 +14,20 @@ int main(int argc, char *argv[])
 
 	cgsize_t range_min, range_max;
 
-	int entry, i, j, k, NX=10, NY=15, NZ=20;
+	int entry, count;
 	double *pressure;
 
 	int field;
+
+	int numberOfTimeSteps, timeStep;
+	double timeInstant, deltaT;
+	char **pressureSolutionName;
+	int solutionIndex;
+	char iterativeBaseName[MAX_STRING_LEN], iterativeZoneName[MAX_STRING_LEN];
+	double *timeArray;
+	cgsize_t timeArrayDimension[1];
+	cgsize_t solutionNameDimension[2];
+	char * solutionNames;
 
 	simpleReadFile(&data);
 	/* Read the grid */
@@ -30,20 +40,6 @@ int main(int argc, char *argv[])
 	gridNumber = 1;
 	err = cg_grid_read(data.file, data.base, data.zone, gridNumber, gridName); CHKERRQ(err);
 
-	/* Create pressure field */
-	pressure = (double *) malloc(data.size[0]*sizeof(double));
-	for(i=0 ; i<NX ; ++i)
-	{
-		for(j=0 ; j<NY ; ++j)
-		{
-			for(k=0 ; k<NZ ; ++k)
-			{
-				entry = i + j*NX + k*NX*NY;
-				pressure[entry] = (double) entry;
-			}
-		}
-	}
-
 	/* Read coordinates */
 	err = cg_ncoords(data.file, data.base, data.zone, &numberOfCoordinates); CHKERRQ(err);
 	x = (double *) malloc(data.size[0]*sizeof(double));
@@ -51,16 +47,58 @@ int main(int argc, char *argv[])
 	z = (double *) malloc(data.size[0]*sizeof(double));
 	range_min = 1;
 	range_max = data.size[0];
-	err = cg_coord_read(data.file, data.base, data.zone, "CoordinateX",CGNS_ENUMV(RealDouble), &range_min, &range_max, x); CHKERRQ(err);
-	err = cg_coord_read(data.file, data.base, data.zone, "CoordinateY",CGNS_ENUMV(RealDouble), &range_min, &range_max, y); CHKERRQ(err);
-	err = cg_coord_read(data.file, data.base, data.zone, "CoordinateZ",CGNS_ENUMV(RealDouble), &range_min, &range_max, z); CHKERRQ(err);
+	err = cg_coord_read(data.file, data.base, data.zone, "CoordinateX", CGNS_ENUMV(RealDouble), &range_min, &range_max, x); CHKERRQ(err);
+	err = cg_coord_read(data.file, data.base, data.zone, "CoordinateY", CGNS_ENUMV(RealDouble), &range_min, &range_max, y); CHKERRQ(err);
+	err = cg_coord_read(data.file, data.base, data.zone, "CoordinateZ", CGNS_ENUMV(RealDouble), &range_min, &range_max, z); CHKERRQ(err);
 
-	/* Write field */
-	err = cg_sol_write(data.file, data.base, data.zone, data.solutionName, CGNS_ENUMV(Vertex), &(data.solution)); CHKERRQ(err);
-	err = cg_field_write(data.file, data.base, data.zone, data.solution, CGNS_ENUMV(RealDouble), "Pressure", pressure, &field); CHKERRQ(err);
+	/* Transient solution */
+	numberOfTimeSteps = 20;
+	deltaT = 1;
+	pressure = (double *) malloc(data.size[0]*sizeof(double));
+	pressureSolutionName = (char **) malloc(numberOfTimeSteps*sizeof(char*));
+	for(timeStep=0 ; timeStep<numberOfTimeSteps ; ++timeStep)
+	{
+		timeInstant = timeStep * deltaT;
+		pressureSolutionName[timeStep] = (char *) malloc(MAX_STRING_LEN * sizeof(char));
+		sprintf(pressureSolutionName[timeStep], "pressure_%02d", timeStep+1); /* Paraview start at one */
+		/*printf("\npressure solution name, time step %d: %s\n\n", timeStep, pressureSolutionName[timeStep]);*/
+		for(count=0 ; count<data.size[0] ; ++count)
+			pressure[count] = count * timeInstant;
 
+		/* Write field */
+		err = cg_sol_write(data.file, data.base, data.zone, pressureSolutionName[timeStep], CGNS_ENUMV(Vertex), &solutionIndex); CHKERRQ(err);
+		err = cg_field_write(data.file, data.base, data.zone, solutionIndex, CGNS_ENUMV(RealDouble), "Pressure", pressure, &field); CHKERRQ(err);
+	}
+
+	/* Create a iterative base */
+	strcpy(iterativeBaseName, "TimeIterativeValues");
+	err = cg_biter_write(data.file, data.base, iterativeBaseName, numberOfTimeSteps); CHKERRQ(err);
+
+	/* Create array of time steps */
+	timeArray = (double*) malloc(numberOfTimeSteps * sizeof(double));
+	timeArrayDimension[0] = numberOfTimeSteps;
+	for(timeStep=0 ; timeStep<numberOfTimeSteps ; ++timeStep)
+		timeArray[timeStep] = timeStep;
+	err = cg_goto(data.file, data.base, iterativeBaseName, 0, "end"); CHKERRQ(err);
+	err = cg_array_write("TimeValues", CGNS_ENUMV(RealDouble), 1, timeArrayDimension, timeArray); CHKERRQ(err);
+
+	/* Create iterative zone */
+	strcpy(iterativeZoneName, "ZoneIterativeData");
+    cg_ziter_write(data.file, data.base, data.zone, iterativeZoneName);
+	err = cg_goto(data.file, data.base, "Zone_t", data.zone, iterativeZoneName, 0, "end"); CHKERRQ(err);
+
+	solutionNameDimension[0] = 11;
+	solutionNameDimension[1] = numberOfTimeSteps;
+	solutionNames = (char*) malloc(100*numberOfTimeSteps*sizeof(char));
+	strcpy(solutionNames, "\0");
+	for(timeStep=0 ; timeStep<numberOfTimeSteps ; ++timeStep)
+		strcat(solutionNames, pressureSolutionName[timeStep]);
+	err = cg_array_write("PressureSolutionPointers", CGNS_ENUMV(Character), 2, solutionNameDimension, solutionNames); CHKERRQ(err);
+
+    err = cg_simulation_type_write(data.file, data.base, CGNS_ENUMV(TimeAccurate)); CHKERRQ(err);
 
 	/* Close file and program */
 	cg_close(data.file);
+	free(pressure);
 	exit(EXIT_SUCCESS);
 }
