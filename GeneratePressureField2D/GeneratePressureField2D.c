d98e2253bb060633feb282d76323de97058fcfee
Lucas Guesser Targino da Silva <guesser@sinmec.ufsc.br>
2018-01-17 10:19:03 -0200

    Project to generate a pressure field in 2D grid complete.

--- GeneratePressureField2D/GeneratePressureField2D.c
+++ GeneratePressureField2D/GeneratePressureField2D.c
@@ -20,29 +20,9 @@ int main(int argc, char *argv[])
 	int field;
 
 	simpleReadFile2(&data, two_dimensional);
-	/* Read the grid */
-	err = cg_ngrids(data.file, data.base, data.zone, &numberOfGrids); CHKERRQ(err);
-	if(numberOfGrids!=1)
-	{
-		fprintf(stderr, "Wrong number of grids. It should be 1!\n\n");
-		cg_error_exit();
-	}
-	gridNumber = 1;
-	err = cg_grid_read(data.file, data.base, data.zone, gridNumber, gridName); CHKERRQ(err);
-
-	/* Read coordinates */
-	err = cg_ncoords(data.file, data.base, data.zone, &numberOfCoordinates); CHKERRQ(err);
-	x = (double *) malloc(data.size[0]*sizeof(double));
-	y = (double *) malloc(data.size[0]*sizeof(double));
-	z = (double *) malloc(data.size[0]*sizeof(double));
-	range_min = 1;
-	range_max = data.size[0];
-	err = cg_coord_read(data.file, data.base, data.zone, "CoordinateX", CGNS_ENUMV(RealDouble), &range_min, &range_max, x); CHKERRQ(err);
-	err = cg_coord_read(data.file, data.base, data.zone, "CoordinateY", CGNS_ENUMV(RealDouble), &range_min, &range_max, y); CHKERRQ(err);
-	err = cg_coord_read(data.file, data.base, data.zone, "CoordinateZ", CGNS_ENUMV(RealDouble), &range_min, &range_max, z); CHKERRQ(err);
 
 	/* Create pressure field */
-	pressure = (double *) malloc(data.size[0]*sizeof(double));
+	pressure = (double *) malloc(data.numberOfVertices*sizeof(double));
 	for(count=0 ; count<data.size[0] ; ++count)
 	{
 		pressure[count] = (double) count;
